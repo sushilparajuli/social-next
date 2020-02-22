0b0d093f4ac0c48960b49258da4b76884eb08c31
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-02-22 14:12:53 +0400

    Initial commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const passport = require("passport");
+
+exports.validateSignup = (req, res, next) => {
+  req.sanitizeBody("name");
+  req.sanitizeBody("email");
+  req.sanitizeBody("password");
+
+  // Name is on-null and is 4-10 characters long
+  req.checkBody("name", "Enter a name").notEmpty();
+  req.checkBody("name", "Name must be between 4-10 characters").isLength({
+    min: 4,
+    max: 10
+  });
+
+  //Email is non-null, valid and normalized
+  req
+    .checkBody("email", "Enter a valid email")
+    .isEmail()
+    .normalizeEmail();
+
+  // Name is on-null and is 4-10 characters long
+  req.checkBody("password", "Enter a password").notEmpty();
+  req.checkBody("password", "Name must be between 4-10 characters").isLength({
+    min: 4,
+    max: 10
+  });
+
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map(error => error.msg)[0];
+    return res.status(400).send(firstError);
+  }
+  next();
+};
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await new User({ name, email, password });
+  await User.register(user, password, (err, user) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json(user.name);
+  });
+};
+
+exports.signin = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json(err.message);
+    }
+
+    if (!user) {
+      return res.status(400).json(info.message);
+    }
+
+    req.logIn(user, err => {
+      if (err) {
+        return res.status(500).json(err.message);
+      }
+      res.json(user);
+    });
+  })(req, res, next);
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("next-cookie.sid");
+  req.logout();
+  res.json({
+    message: "You are logged out successfully"
+  });
+};
+
+exports.checkAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/signin");
+};
